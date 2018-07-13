3cf57e0a859cfe53830523be1a07b5f67d5c1db3
greycabb <greycabbage@gmail.com>
2018-07-12 17:38:40 -0700

    create ui-flow branch

--- Client/src/TopicPage.js
+++ Client/src/TopicPage.js
@@ -13,7 +13,7 @@ export default class TopicPage extends React.Component {
 
         this.state = {
             'mobileView': mobileView
-        };
+        }; 
     }
 
     componentDidMount() {
